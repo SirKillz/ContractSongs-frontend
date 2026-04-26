e91f8be1d8fc7cd4c8afbaf42d0bbca7d3394655
Nick Killeen <sirkillz1198@gmail.com>
2026-04-26 13:20:40 -0500

    Hnadling initial check for API Keys

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Alert, Button } from "@mui/material";
 
 import FixedPage from "@/components/common/FixedPage";
+import Section from "@/components/common/Section";
 
 import { useApiKeys } from "@/hooks/apiKeyManagementHooks";
 
 export default function Home() {
 
-  const {isPending, isError, data, error} = useApiKeys();
+  const {isPending, isError, data} = useApiKeys();
 
   return (
     <FixedPage>
@@ -20,6 +21,29 @@ export default function Home() {
       >
         <Typography component="h1" variant="h1">Welcome to the Spotify Contract Song App!</Typography>
       </Box>
+
+
+      {isError && <Section>
+        <Box sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px"
+        }}>
+          <Alert 
+            severity="error"
+            sx={{display: "flex", justifyContent: "center"}}
+          >
+            No Spotify Credentials Detected, Please Sign In
+          </Alert>
+          <Button 
+            href="https://accounts.spotify.com/authorize?client_id=400ccbc311e24c878036fc6821ec6e98&response_type=code&redirect_uri=http://127.0.0.1:3000&scope=user-read-currently-playing%20user-modify-playback-state%20playlist-read-private"
+            variant="contained"
+            sx={{backgroundColor: "#1ed760", alignSelf: "center"}}
+          >
+            Spotify Sign In
+          </Button>
+        </Box>
+      </Section>}
     </FixedPage>
   );
 }
---
+++ components/common/Section.tsx
@@ -0,0 +1,17 @@
+import { Box } from "@mui/material"
+
+type props = {
+    children: React.ReactNode
+}
+
+export default function Section({children}: props) {
+    return (
+        <Box sx={{
+            borderRadius: "16px",
+            boxShadow: "5px 5px 10px 5px rgba(174, 172, 172, 0.15)",
+            padding: "20px"
+        }}>
+            {children}
+        </Box>
+    )
+}
