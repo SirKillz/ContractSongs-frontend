30d64961fbaa8f6506960a168d1345b684b48fe7
Nick Killeen <sirkillz1198@gmail.com>
2026-04-28 15:34:45 -0500

    Added a Primary color to the theme

--- app/theme.ts
+++ app/theme.ts
@@ -1,6 +1,11 @@
 import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1ed760"
+    }
+  },
   typography: {
     h1: { fontSize: '2rem', fontWeight: 600 },
     h2: { fontSize: '1.75rem' },
