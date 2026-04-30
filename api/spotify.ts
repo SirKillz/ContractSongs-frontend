e1761cf43a422eb24a3d06af82189f770b6ffc40
Nick Killeen <sirkillz1198@gmail.com>
2026-04-30 08:05:18 -0500

    Created the api call and hook for getting spotify playlist songs

--- api/spotify.ts
+++ api/spotify.ts
@@ -1,9 +1,20 @@
 import { callAPI } from "./apiBase";
 
-import { SpotifyPlaylistResponse } from "@/types/spotify";
+import { SpotifyPlaylistResponse, SpotifySong } from "@/types/spotify";
 
 export async function getSpotifyPlaylists(): Promise<SpotifyPlaylistResponse> {
     const url = `${process.env.NEXT_PUBLIC_BASE_API_URL}/spotify/playlists`;
+    const fetchConfig = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    return await callAPI(url, fetchConfig);
+}
+
+export async function getSpotifyPlaylistSongs(playlist_id: string): Promise<SpotifySong[]> {
+    const url = `${process.env.NEXT_PUBLIC_BASE_API_URL}/spotify/playlists/${playlist_id}/songs`
     const fetchConfig = {
         method: "GET",
         headers: {
--- hooks/spotify.tsx
+++ hooks/spotify.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 
-import { getSpotifyPlaylists } from "@/api/spotify";
+import { getSpotifyPlaylists, getSpotifyPlaylistSongs } from "@/api/spotify";
 
 type useSpotifyPlaylistOptions = {
     enabled?: boolean
@@ -10,6 +10,19 @@ export function useGetSpotifyPlaylists({enabled}: useSpotifyPlaylistOptions) {
     return useQuery({
         queryKey: ['spotify', 'playlists'],
         queryFn: getSpotifyPlaylists,
+        staleTime: 1000 * 60 * 5, // 5 minutes
+        enabled: enabled
+    })
+}
+
+type useGetSpotifyPlaylistSongOptions = {
+    enabled?: boolean
+    playlist_id: string
+}
+export function useGetSpotifyPlaylistSongs({enabled, playlist_id}: useGetSpotifyPlaylistSongOptions) {
+    return useQuery({
+        queryKey: ['spotify', 'playlist', playlist_id, 'songs'],
+        queryFn: () => getSpotifyPlaylistSongs(playlist_id),
         staleTime: 1000 * 60 * 5, // 5 minutes
         enabled: enabled
     })
